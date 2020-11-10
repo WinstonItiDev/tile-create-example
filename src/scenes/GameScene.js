ef55a6eedce7a20e0ed8aaae1931fe647d3bef34
winstonshred@gmail.com <winstonshred@gmail.com>
2020-11-10 18:37:31 +1300

    improved point clicks, will now expand tiles

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -26,7 +26,6 @@ export class GameScene extends Phaser.Scene {
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
 
         this.input.on('pointerdown', (pointer) => {
-            console.log("pressing");
         })
     }
 
@@ -35,39 +34,54 @@ export class GameScene extends Phaser.Scene {
         // first declare the ground layer tile as 0
         // to utilize the alpha channel
 
-        map.forEachTile(function (tile) {
-            tile.index = 0
-        });
+        let x = null
+        let y = null
 
-        worldPoint = this.input.activePointer.positionToCamera(this.cameras.main)
-
-        // just like in the phaser 3 get tiles in shape example
-        overlappingTiles = [];
-
-        rect = new Phaser.Geom.Rectangle(0, 0, worldPoint.x, worldPoint.y)
-
-        overlappingTiles = map.getTilesWithinShape(rect, groundLayer)
-
-        // set tile index 
-        overlappingTiles.forEach((tile) => {
-            tile.index = 7
-        })
-
-        if (!p1) {
-            p1 = worldPoint.clone()
+        if (this.input.activePointer.justDown) {
+            let initialPoint = this.input.activePointer.positionToCamera(this.cameras.main)
+            if (!p1) {
+                p1 = initialPoint.clone()
+            } else if (!p2) {
+                // clone
+            } else {
+                // has data
+                // null
+            }
         }
 
+        if (p1) {
+            map.forEachTile(function (tile) {
+                tile.index = 0
+            });
+
+            worldPoint = this.input.activePointer.positionToCamera(this.cameras.main)
+
+            // just like in the phaser 3 get tiles in shape example
+            overlappingTiles = [];
+
+            let xStart = Math.min(p1.x - 5, worldPoint.x);
+            let yStart = Math.min(p1.y - 5, worldPoint.y);
+            let xEnd = Math.max(p1.x - 5, worldPoint.x);
+            let yEnd = Math.max(p1.y - 5, worldPoint.y);
+
+            rect = new Phaser.Geom.Rectangle(xStart, yStart, xEnd - xStart, yEnd - yStart);
+
+            overlappingTiles = map.getTilesWithinShape(rect, groundLayer)
+
+            // set tile index 
+            overlappingTiles.forEach((tile) => {
+                tile.index = 7
+            })
+        }
 
         // map.removeTileAtWorldXY(worldPoint.x, worldPoint.y)
-
-
         // if (this.input.activePointer.justDown) {
         //     p1 = worldPoint.clone()
         //     console.log(p1);
-        //     // xStart = Math.min(0, worldPoint.x);
-        //     // yStart = Math.min(0, worldPoint.y);
-        //     // xEnd = Math.max(worldPoint.x, worldPoint.x);
-        //     // yEnd = Math.max(worldPoint.y, worldPoint.y);
+            // xStart = Math.min(0, worldPoint.x);
+            // yStart = Math.min(0, worldPoint.y);
+            // xEnd = Math.max(worldPoint.x, worldPoint.x);
+            // yEnd = Math.max(worldPoint.y, worldPoint.y);
 
         // if (!p1) {
         //     p1 = worldPoint.clone()
